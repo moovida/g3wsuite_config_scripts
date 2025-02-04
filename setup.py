5f09ecc722db8e05264fb08bdd19248c98f99acb
Andrea Antonello <andrea.antonello@gmail.com>
2025-02-04 15:21:20 +0100

    added branding for dev version

--- setup.py
+++ setup.py
@@ -2,17 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name="g3wsuite_config_scripts",
-    version="0.3.1",
+    version="0.4.0",
     description="Configuration scripts for the g3w suite setup",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
             "g3w_dev_setup=g3w_config_scripts.g3w_dev_setup:main",  
             "g3w_prod_setup=g3w_config_scripts.g3w_prod_setup:main",  
-            "g3w_config_gen=g3w_config_scripts.g3w_config_gen:main",  
+            "g3w_config_gen=g3w_config_scripts.g3w_config_gen:main",
+            "g3w_branding=g3w_config_scripts.g3w_branding:main",
         ],
     },
     install_requires=[
+        "pillow>=11.1.0",
     ],
     python_requires=">=3.7",
     classifiers=[
